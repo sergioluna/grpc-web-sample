fb0b4238f18e7e563cae6b7af2ae2b891a973baf
sergioluna <sluna@sergioluna.dev>
2022-02-14 15:05:56 -0800

    Create echo server

---
+++ server/echo_server.py
@@ -0,0 +1,23 @@
+from concurrent import futures
+
+import grpc
+import echo_pb2
+import echo_pb2_grpc
+
+class EchoServiceServicer(echo_pb2_grpc.EchoServiceServicer):
+
+    def Echo(self, request, context):
+        return request.message
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    echo_pb2_grpc.add_EchoServiceServicer_to_server(
+        EchoServiceServicer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    serve()
